f3bed27718dd7d5ecb1f9f490566ce638ce9f886
Huynh Huu Duc <1712354@student.hcmus.edu.vn>
2020-10-06 15:17:30 +0700

    Updates

---
+++ precache-manifest.5fa8bec0543866d238bfe26aa9c5801d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bed754c917692b40d67c020d03117a20",
+    "url": "/tictactoe-react/index.html"
+  },
+  {
+    "revision": "620b4ae8c65a9430afc6",
+    "url": "/tictactoe-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "caad87b5f2e5caa12354",
+    "url": "/tictactoe-react/static/js/2.97b3d4a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe-react/static/js/2.97b3d4a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "620b4ae8c65a9430afc6",
+    "url": "/tictactoe-react/static/js/main.8e0db641.chunk.js"
+  },
+  {
+    "revision": "514f82430784f25a752b",
+    "url": "/tictactoe-react/static/js/runtime-main.a7ab193d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tictactoe-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
